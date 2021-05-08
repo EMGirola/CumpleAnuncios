03e10e1d94eeabecb0b3d18cc6c3d96142c5cb15
Ezequiel Girola <ezequielgirola@gmail.com>
2021-05-08 01:41:03 -0300

    For now, delete all
    - Heroku doesnt support filesystem, we need to think another solution

--- UserContainer.js
+++ UserContainer.js
@@ -92,11 +92,6 @@ module.exports = class {
     }
 
     saveUsers() {
-        if (!this.timer) {
-            this.timer = setTimeout(this.saveData, 30000);
-            setTimeout(function() {
-                this.timer = null;
-            }, 30100)
-        }
+        this.saveData();
     }
 }
