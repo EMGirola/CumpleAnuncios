314d1c359b86b9adb7e19ab70f5c081cd02bc5c9
EMGirola <ezequielgirola@gmail.com>
2022-02-26 19:18:43 -0300

    Adding schema for postgresql errors

--- TimerRepository.js
+++ TimerRepository.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg');
 
 const TABLE_NAME = "notifications";
+const TABLE_SCHEMA = "public";
 
 module.exports = class {
     
@@ -18,7 +19,7 @@ module.exports = class {
     async fetchLastNotification() {
         this.checkOrCreateTimerTable();
 
-        let result = await this.pool.query(`SELECT id, created_at FROM ${TABLE_NAME} ORDER BY created_at DESC LIMIT 1`);
+        let result = await this.pool.query(`SELECT id, created_at FROM ${TABLE_SCHEMA}.${TABLE_NAME} ORDER BY created_at DESC LIMIT 1`);
 
         console.log('Result fetched from last notification: ', result);
 
@@ -38,7 +39,7 @@ module.exports = class {
 
 
     async checkOrCreateTimerTable() {
-        await this.pool.query(`CREATE TABLE IF NOT EXISTS ${TABLE_NAME} (
+        await this.pool.query(`CREATE TABLE IF NOT EXISTS ${TABLE_SCHEMA}.${TABLE_NAME} (
             id SERIAL,
             created_at TIMESTAMP  
         )`);            
