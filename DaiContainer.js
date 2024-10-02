f3d852458d9c80a72656252e101316f7db8aca16
EMGirola <ezequielgirola@gmail.com>
2024-10-02 18:17:54 -0300

    channel not variable

--- DaiContainer.js
+++ DaiContainer.js
@@ -3,32 +3,32 @@ const amqp = require('amqplib/callback_api');
 const daiWpp = process.env.DAI_WPP;
 const queue = process.env.PROD_QUEUE || 'prod_wpp_queue';
 
+let channelRabbit;
+
+
+amqp.connect(process.env.RABBIT_PRODUCER, function (error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createConfirmChannel(function (error1, channel) {
+        if (error1) {
+            console.log(`Error1?`);
+            throw error1;
+        }
+
+        channel.assertQueue(queue, {
+            durable: true
+        });
+
+
+        channelRabbit = channel;
+    });
+
+});
 
 module.exports = class {
 
     constructor() {
-        this.channelMq = null;
-
-        amqp.connect(process.env.RABBIT_PRODUCER, function (error0, connection) {
-            if (error0) {
-                throw error0;
-            }
-            connection.createConfirmChannel(function (error1, channel) {
-                if (error1) {
-                    console.log(`Error1?`);
-                    throw error1;
-                }
-
-                channel.assertQueue(queue, {
-                    durable: true
-                });
-        
-        
-                this.channelMq = channel;
-            });
-        
-        });
-
     }
 
 
